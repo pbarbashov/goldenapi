cadbcc5da9377059a0f2e6f29a2395a6ab6bf6a4
pbarb <pbarbashov@gmail.com>
2021-04-29 21:22:21 +0300

    deleted

--- service2/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
